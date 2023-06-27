ad82ec9d67dc87aaf1f1ffe750731ca15dd22301
hayward2007 <65775702+hayward2007@users.noreply.github.com>
2023-06-27 16:13:44 +0900

    Base configured

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={style.footer}>
+        <p>Footer</p>
+        </footer>
+    );
+}
+
+export default Footer;
