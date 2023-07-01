49bcfb2c4048d6c5799392f66d971b4b4e8f7eab
hayward2007 <65775702+hayward2007@users.noreply.github.com>
2023-07-02 03:18:15 +0900

    Making Main Page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -6,14 +6,19 @@ import style from "./main.module.css";
 export default function Main() {
 	return (
 	<>
-		<div className={`${style.content} row`} >
+		<div className={`${style.content} row center`} >
 			<div className={`${style.frame} column start`}>
 				<h2>방구석 개발자,</h2>
 				<h1>김형석</h1>
 			</div>
-			<div className={`${style.frame} column end`}>
-			<h2>의 사촌동생,</h2>
-				<h1>김건우</h1>
+			<div className={`${style.frame} column center`}>
+				<div className="column">
+					<h2 style={{height: 200}}>안녕하세요!</h2>
+					<div className="row">
+						<div onClick={() => console.log("Hello World!")} className={style.button}>블로그 보기</div>
+						<div className={style.button}>포트폴리오 보기</div>
+					</div>
+				</div>
 			</div>
 
 			<div className={style.learn}>
