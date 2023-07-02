9c6c37356ee48044ffdb654da6f6ef5a6119f685
hayward2007 <65775702+hayward2007@users.noreply.github.com>
2023-07-03 08:59:53 +0900

    Main Page Made

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-// import Spline from "@splinetool/react-spline";
+import Spline from "@splinetool/react-spline";
 
 import style from "./main.module.css";
 
 export default function Main() {
 	return (
-	<>
+	<>	
 		<div className={`${style.content} row center`} >
+			<Spline className={style.spline} scene="https://prod.spline.design/ukmm6q-uTyxjpejj/scene.splinecode" />
+
+			{/* <div className="row" style={{position: 'fixed', top: 0}}>
+				<div className={style.bar}>HAYWARD_KIM</div>
+			</div> */}
+
 			<div className={`${style.frame} column start`}>
-				<h2>방구석 개발자,</h2>
-				<h1>김형석</h1>
+				<h2 className={style.slidein_animation}>방구석 개발자,</h2>
+				<div className={`${style.title} ${style.slidein_animation}`}>김형석</div>
 			</div>
-			<div className={`${style.frame} column center`}>
+			<div className={`${style.frame} column end`}>
 				<div className="column">
-					<h2 style={{height: 200}}>안녕하세요!</h2>
 					<div className="row">
 						<div onClick={() => console.log("Hello World!")} className={style.button}>블로그 보기</div>
 						<div className={style.button}>포트폴리오 보기</div>
@@ -21,14 +26,18 @@ export default function Main() {
 				</div>
 			</div>
 
-			<div className={style.learn}>
+			<div className={style.learn} onClick={async () => {
+				await window.scroll({top: window.innerHeight, behavior: 'smooth'});
+			}}>
 				더 알아보기
 			</div>
+
 		</div>
 
 		<div className={`${style.content}`} style={{backgroundColor: 'white'}}>
 			
 		</div>
+
 	</>
 	
 	);
