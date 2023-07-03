3a1b4a4389fdedec181cbd6837b81a307187024c
hayward2007 <65775702+hayward2007@users.noreply.github.com>
2023-07-03 18:53:48 +0900

    Animation Made

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,14 +1,45 @@
-// import React from "react";
+import React, { useState, useEffect} from "react";
 import Spline from "@splinetool/react-spline";
 
 import style from "./main.module.css";
 
 export default function Main() {
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const handleScroll = () => {
+		const position = window.scrollY;
+		// console.log(position, window.innerHeight);
+		setScrollPosition(position);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const TextRing = (text: string) => {
+		const CHARS = text.split('')
+		const INNER_ANGLE = 360 / CHARS.length
+		const RADIUS = 2;
+		return (
+		  <span className={style.text_ring} style={{ '--total': CHARS.length, '--radius': RADIUS / Math.sin(INNER_ANGLE / (180 / Math.PI)) } as React.CSSProperties}>
+			{CHARS.map((char, index) => (
+			  <span key={index} style={{'--index': index } as React.CSSProperties}>
+				{char}
+			  </span>
+			))}
+		  </span>
+		)
+	  }
+
+	  
 	return (
 	<>	
+		{/* 메인 페이지 타이틀 */}
 		<div className={`${style.content} row center`} >
 			<Spline className={style.spline} scene="https://prod.spline.design/ukmm6q-uTyxjpejj/scene.splinecode" />
-
 			{/* <div className="row" style={{position: 'fixed', top: 0}}>
 				<div className={style.bar}>HAYWARD_KIM</div>
 			</div> */}
@@ -31,11 +62,39 @@ export default function Main() {
 			}}>
 				더 알아보기
 			</div>
-
 		</div>
-
-		<div className={`${style.content}`} style={{backgroundColor: 'white'}}>
+		{/* 메인 페이지 소개 */}
+		<div className={`${style.content}`}>
+			{
+				scrollPosition === window.innerHeight ?
+				<>
+					<div className={style.left}>
+						<h1 className={style.slidein_animation}>개</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '0.25s'}}>간</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '0.5s'}}>지</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '0.75s'}}>나</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '1s'}}>는</h1>
+					</div>
+					<div className={style.right}>
+						<h1 className={style.slidein_animation} style={{animationDelay: '1.25s'}}>애</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '1.5s'}}>니</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '1.75s'}}>메</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '2s'}}>이</h1>
+						<h1 className={style.slidein_animation} style={{animationDelay: '2.25s'}}>션</h1>
+					</div>
+				</>
+			:null
+			}
 			
+			<span className={`${style.circle} ${style.inner}`}>
+				{ TextRing("HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   ") }
+			</span>
+			<span className={`${style.circle} ${style.middle}`}>
+				{ TextRing("HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   ") }
+			</span>
+			<span className={`${style.circle} ${style.outer}`}>
+				{ TextRing("HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   HAYWARD_KIM   ") }
+			</span>
 		</div>
 
 	</>
